43ae1671f6d1df8474e516bb1e1d8b832a69e252
AlexJora <jora.catalinaa@gmail.com>
2024-04-29 23:47:57 +0200

    DropdownLinks

---
+++ src/components/Navbar/DropdownLinks.jsx
@@ -0,0 +1,31 @@
+export const DropdownLinks = [
+  {
+    name: "MASSAGE",
+    links: [
+      {
+        Head: "MASSAGE OVERVIEW",
+        link: "/massage-overview",
+      },
+      {
+        Head: "THAI MASSAGE WITHOUT OIL",
+        link: "/thai-massage-without-oil",
+      },
+      {
+        Head: "THAI MASSAGE WITH OIL",
+        link: "/thai-massage-with-oil",
+      },
+      {
+        Head: "FOOT REXLEXOLOGY",
+        link: "/foot-reflexology",
+      },
+      {
+        Head: "COUPLE MASSAGE",
+        link: "/couple-massage",
+      },
+      {
+        Head: "OIL MASSAGE",
+        link: "/oil-massage",
+      },
+    ],
+  },
+];
