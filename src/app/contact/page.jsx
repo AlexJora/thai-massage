f1920363a197dc276761794ec40985423f76fd60
AlexJora <jora.catalinaa@gmail.com>
2024-06-07 15:03:07 +0200

    changing header for Contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,9 @@ import Form from "@/components/Form";
 const Contact = () => {
   return (
     <div>
+      <div className="flex items-center justify-center w-full bg-secondary-500 h-44">
+        <h1 className="text-4xl font-semibold text-primary-100">CONTACT</h1>
+      </div>
       <Form />
     </div>
   );
